fcd24904135b9e98175bdc8e7ee4f5afccc27427
Prasanth9379 <167778852+Prasanth9379@users.noreply.github.com>
2024-05-13 20:41:38 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,13 @@
+function foo(){
+    var res=document.getElementById("firstname").value;
+    console.log(res);
+    var mid=document.getElementById("middlename").value;
+    console.log(mid);
+    var las=document.getElementById("lastname").value;
+    console.log(las);
+    var ema=document.getElementById("email").value;
+    console.log(ema);
+    var loc=document.getElementById("location").value;
+    console.log(loc);
+
+}
